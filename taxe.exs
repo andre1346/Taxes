811be82e7292103a34c0d03e3c21d7ed907a79f5
andre1346 <andregirard5@hotmail.com>
2017-02-09 13:33:19 -0500

    debut

--- taxe.exs
+++ taxe.exs
@@ -2,11 +2,9 @@ defmodule Taxes do
    def sum(achats), do: sum(achats, 0)
 
    def sum([], total),  do: total
-   def sum([head | tail], total), do: sum(tail, head+total)
-   
-   
-  def total(x), do: x*2
-    
 
-  end
-  	
+   def sum([head | tail], total), do: sum(tail, head+total) 
+   
+     def total(x), do: x*0.15
+
+end
